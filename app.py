5d896b789e0e74bae259457aa1bf9a9fb7b3cdb2
jamiejamiebobamie <jmccrory@vt.edu>
2020-12-20 12:55:20 -0500

    added backend routes. pushng to heroku.

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, render_template, request
 import pymongo
 from pymongo import MongoClient
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
@@ -9,7 +10,7 @@ MONGO_URI = str(os.environ.get('MONGO_URI'))
 MONGO_URI = "mongodb://127.0.0.1:27017/database"
 mongo = MongoClient(MONGO_URI)
 
-from random import randrange
+from random import randrange, randint
 from flask_cors import CORS, cross_origin
 # public API, allow all requests *
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}}) # change this to only accept from my frontend
@@ -25,69 +26,90 @@ def test_DB():
     db = mongo.database
     drawing_data_collection = db.BvT_drawingdata
 
-    return render_template('index.html', title='Home')
+    new_document = {
+            "vertices" : [['.22','.67676']],
+            "description" : "hi",
+            "likes" : 15,
+        }
+    inserted_ok = drawing_data_collection.insert_one(new_document).acknowledged
 
-# serve 6 random drawings from the database
-@app.route('/api/v1/six-random-drawings',methods=['GET'])
+    return render_template('index.html', title='Home',greeting=inserted_ok)
+
+
+# serve < number > random drawings from database
+@app.route('/api/v1/random-drawings/<number>',methods=['GET'])
 @cross_origin()
-def serve_random_drawings():
+def serve_random_drawings(number):
     db = mongo.database
     drawing_data_collection = db.BvT_drawingdata
-    length = drawing_data_collection.count()
 
-    drawing_data = []
-    for _ in range(6):
-        rand_drawing = drawing_data_collection.find()[randrange(length)]
-        # need to stringify the Mongo Object ID from the drawing object before
-            # appending
-        drawing_data.append(rand_drawing)
-    return {"drawing_data": drawing_data}
+    # print(number)
+    rand_drawings = drawing_data_collection.aggregate([
+      { "$sample": {"size": int(number) }},
+      {"$project": { "_id": { "$toString": "$_id" },
+                      "vertices" : 1,
+                      "description": 1,
+                      "likes": 1}
+       }
+    ])
+    rand_drawings = list(rand_drawings)
+    # print(rand_drawings)
+    return {"drawing_data": rand_drawings}
 
 # serve 20 popular drawings from the database based on likes
 @app.route('/api/v1/twenty-liked-drawings',methods=['GET'])
 @cross_origin()
-def serve_random_drawings():
+def serve_liked_drawings():
     db = mongo.database
     drawing_data_collection = db.BvT_drawingdata
     length = drawing_data_collection.count()
-
     best_drawings = drawing_data_collection.aggregate([
       { "$sort": { "likes": -1} },
-      { "$limit": length/3}
+      { "$limit": length/3},
+      {"$project": { "_id": { "$toString": "$_id" },
+                      "vertices" : 1,
+                      "description": 1,
+                      "likes": 1}
+       }
     ])
-
     drawing_data = []
-    for _ in range(20):
-        rand_best_drawing = best_drawings.find()[randrange(length)]
-        # need to stringify the Mongo Object ID from the drawing object before
-            # appending
+
+    # testing.
+    length = max(length,20)
+
+    for _ in range(length):
+        best_drawings = list(best_drawings)
+        rand_integer = randint(0,len(best_drawings)-1)
+        rand_best_drawing = best_drawings[rand_integer]
         drawing_data.append(rand_best_drawing)
+    # print(drawing_data)
     return {"drawing_data": drawing_data}
 
 # create new drawing document for database
 @app.route('/api/v1/add-drawing-to-db',methods=['POST'])
 @cross_origin()
-def serve_quote_from_input():
+def add_drawing_to_db():
     inserted_ok = False
     data = request.get_json()
     if data:
         # if there are less than 5000 vertices and the description is not empty
             # and the description is not "I drew a...", create a new drawing
             # document
-        if len(data["vertices"]) < 5000
-            and len(data["description"])
-            and data["description"] != "I drew a...":
+        if (len(data["drawingData"]) < 7000
+            and len(data["drawingDescription"])
+            and data["drawingDescription"] != "I drew a..."):
 
             db = mongo.database
             drawing_data_collection = db.BvT_drawingdata
 
-            vertices = data["vertices"]
-            description = data["description"]
+            vertices = data["drawingData"]
+            description = data["drawingDescription"]
             likes = 0
 
             new_document = {
                     "vertices" : vertices,
-                    "description" : description,
+                    # a space needs to be added here for frontend...
+                    "description" : description + " ",
                     "likes" : likes,
                 }
             inserted_ok = drawing_data_collection.insert_one(new_document).acknowledged
@@ -95,23 +117,29 @@ def serve_quote_from_input():
 
 # increment the likes of the drawings.
 @app.route('/api/v1/increment-likes',methods=['POST'])
-@cross_origin()
-def serve_quote_from_input():
+# @cross_origin()
+def increment_likes():
     successes = [False]
-    data = mongo.get_json()
+    data = request.get_json()
+    # data = {'IDs':["5fdf62a23bb1ca7d7473f476","5fdf63173bb1ca7d7473f477"]}
     if data:
-        if len(data["IDs"]):
-            db = client.database
+        print(data)
+        _ids = data["_ids"]
+        if _ids:
+            db = mongo.database
             drawing_data_collection = db.BvT_drawingdata
-            for i in range(len(data["IDs"])):
-                drawing_document = drawing_data_collection.find({"_id": ObjectId(data["drawingID"])})
-                current_number_of_likes = drawing_document["likes"]
-                success = cardstacks.update_one( {"_id": ObjectId(data["drawingID"])},
-                    {  "$set": {"likes": current_number_of_likes + 1 }  }).acknowledged
-                if i == 0:
-                    successes[0] = success
-                else:
-                    successes.append(success)
+            for i in range(len(_ids)):
+                object_id = _ids[i]
+                drawing_document = drawing_data_collection.find_one({"_id": ObjectId(object_id) })
+                if drawing_document:
+                    likes = int(drawing_document["likes"])
+                    likes += 1
+                    success = drawing_data_collection.update_one( {"_id": ObjectId(object_id) },
+                        {  "$set": { "likes": str(likes) }  }).acknowledged
+                    if i == 0:
+                        successes[0] = success
+                    else:
+                        successes.append(success)
     return { "success": all(successes) }
 
 
